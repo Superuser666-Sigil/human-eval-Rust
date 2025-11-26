053959c474ed0b8da8eebdb22c9857ba438a55ad
davet <davetmire85@gmail.com>
2025-11-26 15:26:28 -0600

    Bump version to 1.3.1 - Fix rust_execution module export

--- human_eval/__init__.py
+++ human_eval/__init__.py
@@ -4,8 +4,14 @@ HumanEval Rust evaluation package.
 Provides evaluation harness for the HumanEval Rust problem solving dataset.
 
 Copyright (c) 2025 Dave Tofflemire, SigilDERG Project
-Version: 1.3.0
+Version: 1.3.1
 """
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
+
+# Export rust_execution module so it can be imported
+# Use relative import to avoid circular dependency issues
+from . import rust_execution
+
+__all__ = ["rust_execution", "__version__"]
 
